c240f6b1ec7f2f228e484b3287e528ce831d9bf7
“za-zhengyuanqian” <zhengyuanqian@zhongan.io>
2018-07-13 10:46:24 +0800

    first commit

---
+++ JHBuriedPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHBuriedPoint
+//
+//  Created by muma on 2018/7/13.
+//  Copyright © 2018年 muma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHBuriedPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHBuriedPoint
+//
+//  Created by muma on 2018/7/13.
+//  Copyright © 2018年 muma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
